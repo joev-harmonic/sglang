c26d66313d295baddfdf4581b18318c7c0f200d4
Lawrence Wu <lawrence.wu@harmonic.fun>
2026-06-08 07:59:49 +0000

    fix(pd): do not abort when req.disagg_prefill_dp_rank is used
    Commit 3600465 assumes that there are two possibilities:
    * follow_bootstrap_room is used, the dp rank should match the bootstrap
      dp rank, and no register is needed
    * follow_bootstrap_room is not used, and register is not needed
    
    There is a third possibility from commit 539f772:
    * req.disagg_prefill_dp_rank is used, and register is not needed
    
    Currently, if we try to explicit load balance using
    req.disagg_prefill_dp_rank, it will abort due to not matching bootstrap
    dp rank.

--- python/sglang/srt/disaggregation/mori/conn.py
+++ python/sglang/srt/disaggregation/mori/conn.py
@@ -1231,8 +1231,16 @@ class MoriKVSender(CommonKVSender):
         bootstrap_room: int,
         dest_tp_ranks: List[int],
         pp_rank: int,
+        req_has_disagg_prefill_dp_rank: bool = False,
     ):
-        super().__init__(mgr, bootstrap_addr, bootstrap_room, dest_tp_ranks, pp_rank)
+        super().__init__(
+            mgr,
+            bootstrap_addr,
+            bootstrap_room,
+            dest_tp_ranks,
+            pp_rank,
+            req_has_disagg_prefill_dp_rank,
+        )
         self.transfer_statuses: List[TransferStatus] = []
         self.pending_infos: Optional[List[TransferInfo]] = None
         self.sent_last_chunk = False
